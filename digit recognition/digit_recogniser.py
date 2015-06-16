c3433964e2f7fb405d9c8911af8048e108223047
BHouwens <zhion360@gmail.com>
2015-06-16 12:00:42 +0200

    Complete digit recogniser

--- digit recognition/digit_recogniser.py
+++ digit recognition/digit_recogniser.py
@@ -1,7 +1,10 @@
+import csv
 from sklearn.ensemble import RandomForestClassifier
 from setup_data import Data
 
 data = Data()
+counter = 1
+final = [['ImageId', 'Label']]
 
 # set up the pred
 clf = RandomForestClassifier(n_estimators=500, min_samples_split=6,n_jobs=-1)
@@ -9,4 +12,11 @@ clf.fit(data.features_train, data.labels_train)
 
 pred = clf.predict(data.features_test)
 
-print pred[0]
+for entry in pred:
+	final.append([counter, entry])
+	counter += 1
+
+with open('submission.csv', 'wb') as f:
+    writer = csv.writer(f)
+    for val in final:
+        writer.writerow(val)
